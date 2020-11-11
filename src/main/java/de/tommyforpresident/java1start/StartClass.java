95ed6a5f56281f83d975cfd3616f229b467398d7
Thom-Mon <thomas.gebel@fh-erfurt.de>
2020-11-11 11:10:26 +0100

    First Steps of Introduction and Setting Up

---
+++ src/main/java/de/tommyforpresident/java1start/StartClass.java
@@ -0,0 +1,9 @@
+package de.tommyforpresident.java1start;
+
+public class StartClass {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Hello World");
+        }
+}
---
+++ src/test/java/de/tommyforpresident/java1start/StartClassTest.java
@@ -0,0 +1,12 @@
+package de.tommyforpresident.java1start;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
